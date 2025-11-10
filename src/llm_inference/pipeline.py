7528461bbdd36ef8cae257442fdff92b17a7dba9
Kevboyz <kevboyz@pm.me>
2025-11-10 16:55:49 -0300

    fix paths

--- src/llm_inference/pipeline.py
+++ src/llm_inference/pipeline.py
@@ -3,7 +3,7 @@ from s3_summarization import main as run_s3
 from s2_llm_chain import run_llm_chain, load_csv_data
 from s1_make_llm_input import input_hierarquical_code_analysis, input_hierarquical_code_judgement
 from s0_utils import load_prompt
-from paths import PREPROCESSED_POSTS, CODE_ANALYSIS, CODE_JUDGEMENT
+from paths import *
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -12,7 +12,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 def run_full_pipeline(limit=0):
     run_llm_chain(
         input_file=PREPROCESSED_POSTS,
-        output_file=CODE_ANALYSIS,
+        output_file=HIER_LLM_CLASSIFICATION,
         prompt_template=load_prompt("code_analysis_v1.txt", 'h'),
         process_case_func=input_hierarquical_code_analysis,
         data_loader=load_csv_data,
@@ -21,8 +21,8 @@ def run_full_pipeline(limit=0):
         description='Analysing codes'
     )
     run_llm_chain(
-        input_file=CODE_ANALYSIS,
-        output_file=CODE_JUDGEMENT,
+        input_file=HIER_LLM_CLASSIFICATION,
+        output_file=HIER_CODE_JUDGEMENT,
         prompt_template=load_prompt("code_judge_v1.txt", 'h'),
         process_case_func=input_hierarquical_code_judgement,
         limit=limit,
